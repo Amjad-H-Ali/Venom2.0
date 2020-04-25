7d798251a6fb035d05668514342c66ca65335c92
Amjad Ali <aali212@uic.edu>
2020-04-25 20:00:44 +0000

    Tested Lexer with very large 6B  number 0xFFFFFFFFFFFF; the largest number that can be represented in Venom as of now

--- test/lexer_test.cpp
+++ test/lexer_test.cpp
@@ -7,8 +7,10 @@ TEST(LexerTest, PositiveNumbers) {
     uint64_t tokens[SIZE] = {0};
     uint64_t end_of_tokens_list = lexer(tokens);
     
-    uint64_t test_tokens[SIZE] = {0};
-    test_tokens[W_TOKEN_INIT] = 0x7B00 | NUM;
+    uint64_t test_tokens[SIZE]  = {0};
+    test_tokens[W_TOKEN_INIT]   = (static_cast<uint64_t>(NUM) << 56) | static_cast<uint64_t>(0x7B00) | NUM;
+    test_tokens[W_TOKEN_INIT+1] = (static_cast<uint64_t>(0xFF) << 56)|(static_cast<uint64_t>(NUM) << 48) | 0x5f5e100;
+    test_tokens[W_TOKEN_INIT+2] = static_cast<uint64_t>(0xFFFFFFFFFF);
     
     for(size_t i = W_TOKEN_INIT; i < end_of_tokens_list; ++i) {
         ASSERT_EQ(tokens[i],test_tokens[i]);
