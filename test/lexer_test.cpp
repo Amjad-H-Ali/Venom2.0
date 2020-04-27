9a0dffd9dea658f0201742157660636d34913e89
Amjad Ali <aali212@uic.edu>
2020-04-27 03:47:19 +0000

    Wrote test case for variable that begans with letters then numbers then letters

--- test/lexer_test.cpp
+++ test/lexer_test.cpp
@@ -30,7 +30,7 @@ TEST(LexerTest, NumbersAndVariables) {
     uint64_t tokens[SIZE] = {0};
     uint64_t end_of_tokens_list = lexer(tokens);
     
-    ASSERT_EQ(11, end_of_tokens_list);
+    ASSERT_EQ(13, end_of_tokens_list);
     
     std::vector<uint64_t> test_tokens(SIZE, 0);
     
@@ -84,8 +84,21 @@ TEST(LexerTest, NumbersAndVariables) {
         (static_cast<uint64_t>('a') << 48) | (static_cast<uint64_t>('z') << 56);
     
     test_tokens[W_TOKEN_INIT+8] = 
-        // continued
-        '7' | (static_cast<uint64_t>('7') << 8) | (static_cast<uint64_t>('7') << 16);
+        // continued...
+        '7' | (static_cast<uint64_t>('7') << 8) | (static_cast<uint64_t>('7') << 16) |
+        // bongo123bang == VAR
+        (static_cast<uint64_t>(VAR) << 24) | (static_cast<uint64_t>(0xc) << 32) | (static_cast<uint64_t>('b') << 40) |
+        (static_cast<uint64_t>('o') << 48) | (static_cast<uint64_t>('n') << 56);
+    
+    test_tokens[W_TOKEN_INIT+9] =
+        // continued...
+        'g' | (static_cast<uint64_t>('o') << 8) | (static_cast<uint64_t>('1') << 16) | (static_cast<uint64_t>('2') << 24) |
+        (static_cast<uint64_t>('3') << 32) | (static_cast<uint64_t>('b') << 40) | (static_cast<uint64_t>('a') << 48) |
+        (static_cast<uint64_t>('n') << 56);
+    
+    test_tokens[W_TOKEN_INIT+10] =
+        // continued...
+        'g';
     
     for(size_t i = W_TOKEN_INIT; i < end_of_tokens_list; ++i) {
         EXPECT_EQ(test_tokens[i], tokens[i]) << std::hex << "test_tokens[i]: " << test_tokens[i] << '\n' << "tokens[i]: " << tokens[i];
